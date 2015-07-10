4facddd9bfba1d7e7be540c8f6c14e2d2cbec19d
Joao Rietra <jhlrietra@gmail.com>
2015-07-10 11:35:10 -0300

    Create MyServerSocket.java

---
+++ MyServerSocket.java
@@ -0,0 +1,22 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.SocketException;
+
+public class MyServerSocket implements Closeable {
+
+	
+	public MyServerSocket(int port) 
+	throws SocketException {
+		
+	}
+
+	public MySocket accept() throws SocketException {
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		
+	}
+
+}
