b318d529474bbd05212c7faab1d3494e8a640bef
Joao Rietra <jhlrietra@gmail.com>
2015-07-16 18:27:35 -0300

    Update SocketInputStream.java

--- SocketInputStream.java
+++ SocketInputStream.java
@@ -1,9 +1,11 @@
+package net;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.SocketTimeoutException;
 
-public class SocketInputStream extends InputStream {
+class SocketInputStream extends InputStream {
 	private int i;
 	private byte part;
 	public final int length;
@@ -11,40 +13,53 @@ public class SocketInputStream extends InputStream {
 	private final DatagramPacket packet;
 	private final MySocket master;
 	
-	public SocketInputStream(MySocket master) {
+	public SocketInputStream(MySocket master) throws IOException {
 		super();
 		this.packet = master.newPacket();
-		i = packet.getLength();
 		this.length = packet.getLength();
 		part = -127;
 		this.master = master;
+		i = length;
 	}
 	
 	@Override
 	public int read() throws IOException {
 		byte[] buffer = packet.getData();
-		if(i >= packet.getLength()-1) {
-			java.util.Arrays.fill(buffer, (byte) 0);
-			do{
-				try {
-					System.out.print("receiving...");
-					packet.setLength(length);
-					master.socket.receive(packet);
-					// Confirmation
-					if(master.random()) {
-						packet.setLength(1); // No need to send the whole packet back
-						master.socket.send(packet);
-					}
-				} catch (SocketTimeoutException e) { // did not receive anything
-					buffer[0] = -128; // continue
-				}
-			}while(buffer[0] < part); // Maybe the confirmation was not received in a previous packet
-			System.out.println(" received: " + (int) buffer[0]);
-			part++;
-			i = 0;
+		if(i >= packet.getLength() - 1) {
+			if(part > -127) {
+				return -1;
+			}
+			request();
 		}
 		i++;
 		return buffer[i];
+	}
+	
+	private void request() throws IOException {
+		int t = 0;
+		byte[] buffer = packet.getData();
+		java.util.Arrays.fill(buffer, (byte) 0);
+		System.out.print("receiving..");
+		do{
+			System.out.print(".");
+			try {
+				master.socket.receive(packet);
+				// Confirmation
+				if(!master.random()) {
+					packet.setLength(1); // No need to send the whole packet back
+					master.socket.send(packet);
+					packet.setLength(length);
+				}
+			} catch (SocketTimeoutException e) { // did not receive anything
+				buffer[0] = -128; // continue
+			}
+			master.isConnected(t++);
+		}while(buffer[0] < part); // Maybe the confirmation was not received in a previous packet
+		System.out.println(" received: " + (int) buffer[0]);
+		for(i=length-1; i >= 0 && buffer[i] == 0; i--) {}
+		packet.setLength(i+1);
+		part++;
+		i = 0;
 	}
 	
 	@Override
