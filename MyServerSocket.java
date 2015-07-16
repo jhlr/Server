a5f408ec45b17ad7a82953486a9f554642a3a2fb
Joao Rietra <jhlrietra@gmail.com>
2015-07-15 23:34:11 -0300

    Update MyServerSocket.java

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -1,22 +1,30 @@
 import java.io.Closeable;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class MyServerSocket implements Closeable {
 
+	private final int probability, buffersize;
+	private final DatagramSocket socket;
 	
-	public MyServerSocket(int port) 
+	public MyServerSocket(int port, int probability, int buffersize) 
 	throws SocketException {
-		
+		this.probability = probability;
+		this.buffersize = buffersize;
+		this.socket = new DatagramSocket(port);
 	}
 
-	public MySocket accept() throws SocketException {
-		return null;
+	public MySocket accept() throws SocketException, IOException {
+		DatagramPacket packet = new DatagramPacket(new byte[2], 1);
+		socket.receive(packet);
+		return new MySocket(packet.getSocketAddress(), probability, buffersize, false);
 	}
 
 	@Override
 	public void close() throws IOException {
-		
+		socket.close();
 	}
 
 }
