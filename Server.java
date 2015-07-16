fefc62304c61cede39fa7658d56fed8207cfedaa
Joao Rietra <jhlrietra@gmail.com>
2015-07-15 23:38:30 -0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,23 +1,24 @@
 
 public class Server {
-	public final static int port = 8080, bufferSize = 1025;
-	public final static int probability = 30;
-	public final static byte[] clientAdress = {(byte)192, (byte)168, (byte)1, (byte)103};
-
-	public static void main(String[] arguments) throws Throwable {
-		MyServerSocket welcomeSocket = new MyServerSocket(port);
+	public final static int port = 23456, bufferSize = 1025;
+	public final static int lossProbability = 0;
+	
+	public static void main(String[] arguments) throws Exception {
+		MyServerSocket welcomeSocket = new MyServerSocket(port, lossProbability, bufferSize);
 		try {
 			while (true) {
 				MySocket connectionSocket = welcomeSocket.accept();
+				
+				System.out.print("Connection accepted: ");
+				System.out.println(connectionSocket.getSocketAddress());
+				
 				HttpRequest request = new HttpRequest(connectionSocket);
 				Thread thread = new Thread(request);
-				thread.setDaemon(true);
 				thread.start();
+				thread.join(); ///////////////////////////
 			}
-		} catch(Exception e) {
-			return;
 		} finally {
-			welcomeSocket.close();
+			welcomeSocket.close();			
 		}
 	}
 }
