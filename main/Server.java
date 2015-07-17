af313ac7390b5fcd696fa757d992df4938a01fda
Joao Rietra <jhlrietra@gmail.com>
2015-07-16 21:56:24 -0300

    Update Server.java

--- main/Server.java
+++ main/Server.java
@@ -1,25 +1,33 @@
 package main;
+
 import net.*;
 
 public class Server {
-	public final static int port = 23456, bufferSize = 1025;
-	public final static int lossProbability = 50;
+	public final static int port = 23456, bufferSize = 1025, lossProbability = 50;
 	
 	public static void main(String[] arguments) throws Exception {
 		MyServerSocket welcomeSocket = new MyServerSocket(port, lossProbability, bufferSize);
+		MySocket connectionSocket;
 		try {
 			while (true) {
-				MySocket connectionSocket = welcomeSocket.accept();
-				
-				System.out.print("Connection accepted: ");
-				System.out.println(connectionSocket.getSocketAddress());
-				
-				HttpRequest request = new HttpRequest(connectionSocket);
-				Thread thread = new Thread(request);
-				thread.start();
+				try {
+					connectionSocket = welcomeSocket.accept();
+					
+					System.out.print("Connection accepted: ");
+					System.out.println(connectionSocket.getSocketAddress());
+					
+					HttpRequest request = new HttpRequest(connectionSocket);
+					Thread thread = new Thread(request);
+					thread.start();
+					//thread.join();
+					
+				} catch(Exception e) {
+					System.out.println();
+					e.printStackTrace();
+				}
 			}
 		} finally {
-			welcomeSocket.close();			
+			welcomeSocket.close();
 		}
 	}
 }
