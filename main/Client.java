51c7998b66567803e6ee96ab6e0459dab625fa8c
Joao Rietra <jhlrietra@gmail.com>
2015-07-16 22:25:39 -0300

    Update Client.java

--- main/Client.java
+++ main/Client.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Client {
 	public final static int bufferSize = 1025, serverPort = 23456, lossProbability = 50;
 	public final static byte[] serverAdress = {(byte)192, (byte)168, (byte)1, (byte)101};
-	public final static String filename = "./GET.txt";
+	public final static File inputFile = new File("./GET.txt");
 	
 	public static void main(String[] args) throws Exception {
 		MySocket socket = new MySocket(
@@ -22,7 +22,7 @@ public class Client {
 
 		PrintStream serverOut = new PrintStream(socket.getOutputStream());
 		Scanner serverIn = new Scanner(socket.getInputStream());
-		Scanner sc = new Scanner(new FileReader(new File(filename)));
+		Scanner sc = new Scanner(new FileReader(inputFile));
 		try {
 			while(sc.hasNextLine()){
 				serverOut.println(sc.nextLine()); // send something
