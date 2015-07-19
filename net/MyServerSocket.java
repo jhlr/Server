e94a59fb84976c1e7dee46cda47927a65b26bbe4
Joao Rietra <jhlrietra@gmail.com>
2015-07-18 22:55:04 -0300

    Update MyServerSocket.java

--- net/MyServerSocket.java
+++ net/MyServerSocket.java
@@ -15,12 +15,16 @@ public class MyServerSocket implements Closeable {
 	throws SocketException {
 		this.probability = probability;
 		this.buffersize = buffersize;
-		this.socket = new DatagramSocket(port);
+		this.socket = new DatagramSocket(port); 
+		// socket at a defined port to get new clients
 	}
 
 	public MySocket accept() throws SocketException, IOException {
 		DatagramPacket packet = new DatagramPacket(new byte[2], 1);
-		socket.receive(packet);
+		socket.receive(packet); // receive a packet from an unknown address
+		// packet now has client's address
+		// this socket will be created at an available port to listen this specific client
+		// a confirmation packet will be sent to the client to tell what port it is
 		return new MySocket(packet.getSocketAddress(), probability, buffersize, false);
 	}
 
